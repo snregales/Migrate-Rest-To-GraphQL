4a469ca47a44d2fef9ff17551644ace42e3c9f28
Sharlon Regales <sharlonregales+dev@gmail.com>
2020-03-14 16:10:36 +0100

    base configuration
    Base model configurations

--- src/snippets/__init__.py
+++ src/snippets/__init__.py
@@ -3,6 +3,8 @@ from typing import List, Tuple
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
 
+from utils import TimeStampConst
+
 LEXER_ALIASES = 1
 LEXER_NAME = 0
 # for all lexers that have aliases, retrieve its alias and name in sorted order
@@ -11,3 +13,12 @@ LANGUAGE_CHOICES: List[Tuple[str, str]] = sorted(
         lambda lexer: (lexer[LEXER_ALIASES][0], lexer[LEXER_NAME]) if lexer[LEXER_ALIASES] else None,
         get_all_lexers()))
 STYLE_CHOICES: List[Tuple[str, str]] = sorted(map(lambda style: (style, style), get_all_styles()))
+
+
+class SnippetConst(TimeStampConst):
+    OWNER: str = 'owner'
+    TITLE: str = 'title'
+    CODE: str = 'code'
+    HTML: str = 'html'
+    LANG: str = 'language'
+    STYLE: str = 'style'
--- src/snippets/admin.py
+++ src/snippets/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from snippets.models import Snippet
+from .models import Snippet
 
 admin.site.register(Snippet)
