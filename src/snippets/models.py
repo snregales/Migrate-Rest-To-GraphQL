95422275425aaefe9c618bc554cc1a6238a3681e
Sharlon Regales <sharlonregales+dev@gmail.com>
2020-03-14 15:52:12 +0100

    Refactor snippet model
    adjust snippet to use the costume user model

--- src/snippets/models.py
+++ src/snippets/models.py
@@ -1,18 +1,16 @@
-from typing import List
+from typing import Tuple
 
 from django.db import models
 
+from misc import user_foreign_key
 from snippets import LANGUAGE_CHOICES, STYLE_CHOICES
 from snippets.manager import SnippetManager
-from utils.abstract_models import TimeStamp
+from utils.abstract_models import TimeStamp, ReprMixin
 
 
-class Snippet(TimeStamp):
+class Snippet(TimeStamp, ReprMixin):
     """Snippet model"""
-    owner = models.ForeignKey(
-        'auth.User',
-        on_delete=models.CASCADE,
-        related_name='snippets')
+    owner = user_foreign_key(related_name='snippets')
     title = models.CharField(max_length=100)
     code = models.TextField()
     html = models.TextField()
@@ -28,10 +26,7 @@ class Snippet(TimeStamp):
     objects: models.Manager = SnippetManager()
 
     class Meta:
-        ordering: List[str] = ['created', 'updated']
+        ordering: Tuple[str] = ('created', 'updated')
 
     def __str__(self) -> str:
         return self.title
-
-    def __repr__(self) -> str:
-        return f'<{self.__class__.__name__}({self.__str__()!r})>'
